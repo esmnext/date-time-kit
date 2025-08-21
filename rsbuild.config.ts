f2239ef888f52bcbf6bff58db131c1318818e402
jerrychan7 <czy-ken@163.com>
2025-08-21 23:12:45 +0800

    feat: 编译结果区分开源组件和示例代码 & 类型输出到独立文件夹

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginDts } from 'rsbuild-plugin-dts';
 
 export default defineConfig({
+    source: {
+        entry: {
+            // 开源组件入口
+            index: {
+                import: './src/export.ts',
+                html: false,
+            },
+            // 示例页面入口
+            'examples/index': './src/examples/index.ts'
+        }
+    },
     html: {
         template: './src/examples/index.html',
         scriptLoading: 'module'
@@ -15,6 +26,11 @@ export default defineConfig({
                 library: {
                     type: 'module'
                 },
+            },
+            optimization: {
+                splitChunks: {
+                    chunks: 'async' // 只对异步加载的模块进行代码分割
+                }
             },
             experiments: {
                 css: true,
