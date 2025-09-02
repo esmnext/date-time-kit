313f64f71348293f233cc75caff93ab26feb7c2e
jerrychan7 <czy-ken@163.com>
2025-09-02 14:17:00 +0800

    chore(package): 更新版本号至 1.1.10，默认导出的是web component

--- src/web-component-export.ts
+++ src/web-component-export.ts
@@ -4,3 +4,9 @@ export * as HhMmSsMsListGrp from '@/components/hhmmss-ms-list-grp';
 export * as YyyyMmDdListGrp from '@/components/yyyymmdd-list-grp';
 export * as QuickSelector from '@/components/quick-selector';
 export * as Popover from '@/components/popover';
+
+export {
+    default as i18n,
+    type Lang,
+    type kitI18n,
+} from "./i18n";
